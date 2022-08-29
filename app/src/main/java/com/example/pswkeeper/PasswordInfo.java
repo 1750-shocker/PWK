d53c35a28e2ebcb91f4822fc1a7a83fa474601c1
Freddy <2215991508@qq.com>
2022-08-30 04:25:10 +0800

    添加PWKsqlite项目文件

---
+++ app/src/main/java/com/example/pswkeeper/PasswordInfo.java
@@ -0,0 +1,17 @@
+package com.example.pswkeeper;
+
+public class PasswordInfo {
+    public long rowid;
+    public int xuhao;
+    public String about;
+    public String account;
+    public String password;
+
+    public PasswordInfo() {
+        rowid = 0L;
+        xuhao = 0;
+        about = "";
+        account = "";
+        password = "";
+    }
+}
